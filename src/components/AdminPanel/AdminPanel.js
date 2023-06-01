63558783a91114dd921a0c8428f7eb5e777b5fb4
lamimershed <lamimershed04@gmail.com>
2023-06-01 17:35:33 +0530

    first commit

---
+++ src/components/AdminPanel/AdminPanel.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./AdminPanel.css";
+import Navbar from "../Navbar/Navbar";
+import { Navigate } from "react-router-dom";
+
+function AdminPanel(props) {
+    if (props.currentUser.username !== "admin") {
+        alert("Access Denide");
+        return <Navigate to="/" />;
+    }
+    return (
+        <div className="AdminPanel">
+            <div className="adminPanel-cont">
+                <h1>User Lists</h1>
+                <div className="AdminPanel-userlist">
+                    {props.userlist.map((ele) => (
+                        <div key={ele.id} className="AdminPanel-user">
+                            {ele.username}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AdminPanel;
